0d5b52ddc304fe4acb7a68ea7e1bf5fddc2cf20a
1643850774@qq.com <Wanguku@123>
2022-07-11 21:41:06 +0800

    kafka

---
+++ elastic_job/src/test/java/com/example/elastic_job/ElasticJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elastic_job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
